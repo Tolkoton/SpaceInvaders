4d97022eb52fb861d0f92da2c0ad693ef0065808
Myron Leskiv <dimmw@yahoo.com>
2022-05-13 16:38:37 +0300

    init
    init

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame
+import os
+import time
+import random
+pygame.font.init()
+
+#game window settings
+WIDTH, HEIGHT = 800, 1200
+GAME_WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space invaders test")
+
+#load ship images
+RED_SPACE_SHIP = pygame.image.load((os.path.join("assets", "pixel_ship_red_small.png")))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+# Load laser images
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+#Background
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+    level = 0
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
+    lost_font = pygame.font.SysFont("comicsans", 60)
+
+    def redraw_window():
+        GAME_WINDOW.blit(BG, (0, 0))
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+main()
